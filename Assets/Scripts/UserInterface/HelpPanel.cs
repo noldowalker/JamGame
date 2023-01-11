731e4194e4075d9a6a73ef88e841872f338acc0c
noldo_walker <noldowalker@gmail.com>
2023-01-11 20:17:16 +0300

    Interface beautified

--- Assets/Scripts/UserInterface/HelpPanel.cs
+++ Assets/Scripts/UserInterface/HelpPanel.cs
@@ -7,15 +7,6 @@ public class HelpPanel : HidablePanel
 {
     private void Awake()
     {
-        Debug.Log("AWAKE!");
-        var rectTransform = GetComponent<RectTransform>();
-        var newWidth = Screen.width - 100;
-        var newHeight = Screen.height - 100;
-        var rectTransformSizeDelta = rectTransform.sizeDelta;
-        rectTransformSizeDelta.x = newWidth;
-        rectTransformSizeDelta.y = newHeight;
-        rectTransform.sizeDelta = rectTransformSizeDelta;
-        
         base.Awake();
     }
 }
